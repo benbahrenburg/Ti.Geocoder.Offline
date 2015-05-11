0d7654a7a64a93842f27ca5bf8c4257bed87657a
Ben Bahrenburg <ben.bahrenburg@gmail.com>
2015-05-10 19:19:19 -0700

    add examples

--- example/app.js
+++ example/app.js
@@ -1,39 +1,16 @@
-// This is a test harness for your module
-// You should do something interesting in this harness
-// to test out the module and to provide instructions
-// to users on how to use it by example.
 
 
-// open a single window
-var win = Ti.UI.createWindow({
-	backgroundColor:'white'
-});
-var label = Ti.UI.createLabel();
-win.add(label);
-win.open();
-
-// TODO: write your module tests here
 var tigeocoderoffline = require('ti.geocoder.offline');
-Ti.API.info("module is => " + tigeocoderoffline);
 
-label.text = tigeocoderoffline.example();
+tigeocoderoffline.registerCountryInfo({
+	url:  'countries.geo.json'
+});
 
-Ti.API.info("module exampleProp is => " + tigeocoderoffline.exampleProp);
-tigeocoderoffline.exampleProp = "This is a test value";
+tigeocoderoffline.registerTerritoryInfoForCountryCode({
+	countryCode:'US',
+	url: 'us.states.geo.json'
+});
 
-if (Ti.Platform.name == "android") {
-	var proxy = tigeocoderoffline.createExample({
-		message: "Creating an example Proxy",
-		backgroundColor: "red",
-		width: 100,
-		height: 100,
-		top: 100,
-		left: 150
-	});
-
-	proxy.printMessage("Hello world!");
-	proxy.message = "Hi world!.  It's me again.";
-	proxy.printMessage("Hello world!");
-	win.add(proxy);
-}
-
+tigeocoderoffline.reverseGeocoder(40.75773,-73.985708,function(e){
+	console.log(JSON.stringify(e));
+});
